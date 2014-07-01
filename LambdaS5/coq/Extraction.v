0a91dcd7920dcbaaff3e99fed0772245044f9ed1
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2014-07-01 11:05:39 +0200

    Remove exception raised when loading LibLogic.

--- LambdaS5/coq/Extraction.v
+++ LambdaS5/coq/Extraction.v
@@ -6,6 +6,9 @@ Require Import ExtrOcamlBasic.
 Require Import ExtrOcamlNatInt.
 Require Import ExtrOcamlString.
 
+(* We do not want “raise Not_found” to be at the toplevel. *)
+Extraction Inline LibLogic.arbitrary.
+
 Extract Inductive Fappli_IEEE.binary_float => float [
   "(fun s -> if s then (0.) else (-0.))"
   "(fun s -> if s then infinity else neg_infinity)"
