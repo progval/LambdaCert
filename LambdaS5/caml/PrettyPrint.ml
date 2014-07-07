e0d43e36d677d72f0780e772e9e889982c1a3d6c
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2014-07-07 13:42:17 +0200

    Limit depth in PrettyPrint.ml.

--- LambdaS5/caml/PrettyPrint.ml
+++ LambdaS5/caml/PrettyPrint.ml
@@ -1,42 +1,45 @@
-let rec string_of_value_loc st loc =
-  match (Values.get_value_from_store st loc) with
-    | None -> "<reference to non-existing value>"
-    | Some v -> string_of_value st v
-and string_of_value st = function
+let rec string_of_value_loc depth st loc =
+  if (depth = 0) then
+    "<cut>"
+  else
+    match (Values.get_value_from_store st loc) with
+      | None -> "<reference to non-existing value>"
+      | Some v -> string_of_value (depth-1) st v
+and string_of_value depth st = function
 | Values.Null -> "null"
 | Values.Undefined -> "undefined"
 | Values.Number f -> CoqUtils.implode (JsNumber.to_string f)
 | Values.String s -> CoqUtils.implode s
 | Values.True -> "true"
 | Values.False -> "false"
-| Values.Object ptr -> string_of_object_ptr st ptr
+| Values.Object ptr -> string_of_object_ptr depth st ptr
 | Values.Closure (loc_heap, args, body) ->
     Printf.sprintf "<closure func (%s) { %s }>"
       (String.concat ", " (List.map CoqUtils.implode args))
-      (string_of_expression body)
-and string_of_value_loc_option st = function
-| Some v -> string_of_value_loc st v
+      (string_of_expression depth body)
+and string_of_value_loc_option depth st = function
+| Some v -> string_of_value_loc depth st v
 | None -> "<unset val>"
 
-and string_of_object_ptr st ptr =
+and string_of_object_ptr depth st ptr =
   match (Values.get_object_from_store st ptr) with
     | None -> "<reference to non-existing object>"
-    | Some obj -> string_of_object st obj
-and string_of_object st obj =
+    | Some obj -> string_of_object depth st obj
+and string_of_object depth st obj =
   Printf.sprintf "{[#proto: %s, #class: %s, #extensible: %B, #primval: %s, #code: %s] %s}"
-  (string_of_value_loc st obj.Values.object_proto) (CoqUtils.implode obj.Values.object_class)
-  (obj.Values.object_extensible) (string_of_value_loc_option st obj.Values.object_prim_value)
-  (string_of_expression_option obj.Values.object_code)
+  (string_of_value_loc depth st obj.Values.object_proto) (CoqUtils.implode obj.Values.object_class)
+  (obj.Values.object_extensible) (string_of_value_loc_option depth st obj.Values.object_prim_value)
+  (string_of_expression_option depth obj.Values.object_code)
   (String.concat ", " (List.map
-      (fun (name, attr) -> Printf.sprintf "'%s': %s" (CoqUtils.implode name) (string_of_attr st attr))
+      (fun (name, attr) -> Printf.sprintf "'%s': %s" (CoqUtils.implode name) (string_of_attr depth st attr))
       (Values.Heap.to_list obj.Values.object_properties_)))
 
-and string_of_expression e =
+and string_of_expression depth e =
   "<expr>"
-and string_of_expression_option = function
-| Some e -> string_of_expression e
+and string_of_expression_option depth = function
+| Some e -> string_of_expression depth e
 | None -> "<unset expr>"
 
-and string_of_attr st = function
-| Values.Coq_attributes_data_of d -> Values.attributes_data_rect (fun v w c e -> Printf.sprintf "{#value %s, #writable %B, #configurable %B, #enumerable %B}" (string_of_value_loc st v) w c e) d
-| Values.Coq_attributes_accessor_of d -> Values.attributes_accessor_rect (fun g s e c -> Printf.sprintf "{#getter %s, #setter %s}" (string_of_value_loc st g) (string_of_value_loc st s)) d (* enumerable and configurable ignored *)
+and string_of_attr depth st = function
+| Values.Coq_attributes_data_of d -> Values.attributes_data_rect (fun v w c e -> Printf.sprintf "{#value %s, #writable %B, #configurable %B, #enumerable %B}" (string_of_value_loc depth st v) w c e) d
+| Values.Coq_attributes_accessor_of d -> Values.attributes_accessor_rect (fun g s e c -> Printf.sprintf "{#getter %s, #setter %s}" (string_of_value_loc depth st g) (string_of_value_loc depth st s)) d (* enumerable and configurable ignored *)
